d96fe7688b15a9c6ccf06a51640853088a7974c4
Suraj Jha <96387384+btwsuraj@users.noreply.github.com>
2024-12-06 04:11:43 +0530

    Add files via upload

---
+++ formka.php
@@ -0,0 +1,25 @@
+<?php
+$name= $_POST['name'];
+$visitor_email= $_POST['email'];
+$subject= $_POST['subect'];
+$message= $_POST['message'];
+// web name daldena hai yaha pe
+$email_form = 'info@jaykeshavproperty.com';
+$email_subject = 'New Form Submission';
+$email_body = "User Name: $name.\n".
+                "User Email: $visitor_email.\n".
+                    "Subject: $subject.\n".
+                    "User Message: $message.\n";
+
+$to = 'jkpestates@gmail.com';
+
+$headers = "From:$email_form \r\n ";
+
+$headers .= "Reply to: $visitor_email \r\n ";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: contact.html");
+
+
+?>
