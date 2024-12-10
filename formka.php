70ef68a0ec25098522a5f51552e92efe69991cdb
Suraj Jha <96387384+btwsuraj@users.noreply.github.com>
2024-12-10 15:38:34 +0530

    Update formka.php

--- formka.php
+++ formka.php
@@ -1,25 +1,41 @@
 <?php
-$name= $_POST['name'];
-$visitor_email= $_POST['email'];
-$subject= $_POST['subject'];
-$message= $_POST['message'];
-// web name daldena hai yaha pe
-$email_form = 'info@jkpestates.onrender.com';
-$email_subject = 'New Form Submission';
-$email_body = "User Name: $name.\n".
-                "User Email: $visitor_email.\n".
-                    "Subject: $subject.\n".
-                    "User Message: $message.\n";
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-$to = 'jkpestates@gmail.com';
+//Load Composer's autoloader
+require 'PhpMailer/Exception.php';
+require 'PhpMailer/PHPMailer.php';
+require 'PhpMailer/SMTP.php';
 
-$headers = "From:$email_form \r\n ";
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
 
-$headers .= "Reply-To: $visitor_email \r\n ";
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'jkpestates@gmail.com';                     //SMTP username
+    $mail->Password   = 'secrvtrabfdtesgitkpset';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-mail($to,$email_subject,$email_body,$headers);
+    //Recipients
+    $mail->setFrom('jkpestates@gmail.com', 'Jkp Estates');
+    $mail->addAddress('jkpestates@gmail.com', $_POST['name']);     //Add a recipient
+    
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = $_POST["subject"];
+    $mail->Body    = $_POST["message"];
 
-header("Location: contact.html");
-
-
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
 ?>
