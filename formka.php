cb7086003b571300eee9dcb761513cfb41b6dd3d
Suraj Jha <96387384+btwsuraj@users.noreply.github.com>
2024-12-15 14:30:40 +0530

    Update formka.php

--- formka.php
+++ formka.php
@@ -6,9 +6,9 @@ use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 //Load Composer's autoloader
-require 'PhpMailer/Exception.php';
-require 'PhpMailer/PHPMailer.php';
-require 'PhpMailer/SMTP.php';
+require 'PHPMailer/Exception.php';
+require 'PHPMailer/PHPMailer.php';
+require 'PHPMailer/SMTP.php';
 
 //Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
